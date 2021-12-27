3587de788ba47a630424b26d74759f6336ddf051
Aayush Garg <aayushgarg2006@gmail.com>
2021-12-27 15:19:14 +0530

    minor fix to implemenation

--- server.js
+++ server.js
@@ -17,19 +17,22 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD
 const MONGO_USER = process.env.MONGO_USER
 const MONGO_CLUSTER_URL = process.env.MONGO_CLUSTER_URL
 const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME
-const db =  `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER_URL}/${MONGO_DATABASE_NAME}?retryWrites=true&w=majority
-`
+const mongoURI =  `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER_URL}/${MONGO_DATABASE_NAME}?retryWrites=true&w=majority`
 
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(console.log("Connected to Mongo DB"))
 
 
 app.use('/', bloatRouter);
 app.use('/', indexRouter);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+mongoose.connect(String(mongoURI), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((result) => {
+    console.log('Connected to Mongo DB')
+
+    app.listen(PORT, err => {
+        console.log(`App listening on http://localhost:${PORT}`)
+        if (err) throw err
+    })
+}).catch((err) => console.log(err))
+
