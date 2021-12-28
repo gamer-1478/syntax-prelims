ca9edd3b43fec2b731574fe6ba6a720e53298a37
Aayush Garg <aayushgarg2006@gmail.com>
2021-12-28 08:41:14 +0530

    song streaming

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,43 @@
 const indexRouter = require('express').Router();
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    fs = require('fs');
 
 indexRouter.get('/', (req, res) => {
     res.render('index', { title: "Home", description: "Home" })
 });
+
+indexRouter.get('/song_stream/:id', (req, res) => {
+    const filePath = path.join(__dirname, '../songs/' + req.params.id + '.mp3');
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(filePath, { start, end });
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'audio/mpeg',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'audio/mpeg',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(filePath).pipe(res);
+    }
+})
 
 //export routers
 module.exports = indexRouter;
