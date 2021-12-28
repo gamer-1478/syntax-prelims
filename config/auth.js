18ce1f3f13f4a44733a5d77e58b6dd9a8fa46aeb
AlightKnight343 <thegodofgreatness343@gmail.com>
2021-12-28 10:27:50 +0530

    auth

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next();
     }
-    res.redirect('/login');
+    res.redirect('/auth/login');
   },
   forwardAuthenticated: function(req, res, next) {
     if (!req.isAuthenticated()) {
